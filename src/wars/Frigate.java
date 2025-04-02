4ee1ae8530ededdb5bfd759dcf9ff530b51adf98
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-03 02:08:06 +0300

    Update Frigate.java

--- src/wars/Frigate.java
+++ src/wars/Frigate.java
@@ -10,24 +10,46 @@ public class Frigate extends Ship
     private int cannons;
     private int cost;
     
-    public Frigate(String nme, int skillLevel, String captain,int cost, boolean pinnace, int cannons)
+    public Frigate(String nme, int skillLevel, String captain, int cannons)
     {
-        super (nme, skillLevel, captain, 0, "Frigate");
+        super(nme, skillLevel, captain, "Frigate");
         pinnace = false;
         this.cannons = cannons;
+        cost = 0;
     }
     
-    public void setCost(int amount)
+    public void setCost()
     {
-        cost = cannons * 10;
+        int amount = cannons * 10;
+        cost = amount;
+    }
+    
+    public int getCost()
+    {
+        return cost;
+    }
+    
+    public void setPinnace(String pin)
+    {
+       if (pin.equalsIgnoreCase("Yes"))
+       {
+          pinnace = true;
+       } 
+       else 
+       {
+          pinnace = false; 
+       }
     }
     
     @Override
     public String toString()
     {
       String report = super.toString();
+      report += "Cost of ship: " + cost + "\n";
       report += "Pinnace: " + pinnace + "\n";
       report += "Number of cannons: " + cannons + "\n";
       return report;
     }
 }
+
+    
