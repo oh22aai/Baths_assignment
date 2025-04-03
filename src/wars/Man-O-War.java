a9516effe48925f5670c3b540bf3e785405b61de
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-03 17:35:58 +0300

    Update Man-O-War.java
    added the setCost method

--- src/wars/Man-O-War.java
+++ src/wars/Man-O-War.java
@@ -6,45 +6,29 @@ package wars;
  */
 public class Man_O_War extends Ship
 {
-    private int decks;
     private int marines;
-    private int cost;
+    
     
     public Man_O_War(String nme, int skillLevel, String captain, int deck, int marine)
     {
-        super( nme,skillLevel,captain,"Man-O-War", true, false, true);
-        cost = 0;
-        decks = deck;
+        super( nme,skillLevel,captain,"Man-O-War",true, false,true, 0, deck,0);
         marines = marine;
     }
     
     
     public void setCost()
     {
-      if (decks <= 2)
-      {
-          cost = 300;
-      } 
-      else
-      {
-          cost = 500;
-      }
+        super.setCost("Man-O-War");
     }
     
-    public int getCost()
-    {
-      return cost;  
-    }
+    
     
     
     @Override
     public String toString()
     {
       String report = super.toString();
-      report += "Cost of ship: " + cost + "\n";
-      report += "Number of decks: " + decks + "\n";
       report += "Number of marines: " + marines + "\n";
       return report;
     }
-}
-     
+}  
