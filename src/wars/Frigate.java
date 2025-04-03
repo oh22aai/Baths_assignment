71a455ed256a33b55e91b27864b28a62da0a9994
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-03 17:34:18 +0300

    Update Frigate.java
    added the setCost method

--- src/wars/Frigate.java
+++ src/wars/Frigate.java
@@ -7,27 +7,22 @@ package wars;
 public class Frigate extends Ship 
 {
     private boolean pinnace;
-    private int cannons;
-    private int cost;
+    
+    
     
     public Frigate(String nme, int skillLevel, String captain, int cannons)
     {
-        super(nme, skillLevel, captain, "Frigate", true, true, true);
+        super(nme, skillLevel, captain, "Frigate", true, true, true,0,0, cannons );
         pinnace = false;
-        this.cannons = cannons;
-        cost = 0;
+        
+        
     }
     
     public void setCost()
     {
-        int amount = cannons * 10;
-        cost = amount;
+        super.setCost("Frigate");
     }
     
-    public int getCost()
-    {
-        return cost;
-    }
     
     public void setPinnace(String pin)
     {
@@ -45,11 +40,10 @@ public class Frigate extends Ship
     public String toString()
     {
       String report = super.toString();
-      report += "Cost of ship: " + cost + "\n";
       report += "Pinnace: " + pinnace + "\n";
-      report += "Number of cannons: " + cannons + "\n";
       return report;
     }
 }
+
 
     
