04d31517119e6cb0655a8a4dc777d61f8777eea7
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-03 17:31:57 +0300

    Update Ship.java
    changed the cost field in Ship and the setCost method

--- src/wars/Ship.java
+++ src/wars/Ship.java
@@ -10,17 +10,26 @@ public class Ship {
     private int skillLevel;
     private String captain;
     private String type;
-    private boolean battle;
-    private boolean skirmish;
-    private boolean blockade;
+    private boolean battle ;
+    private boolean skirmish ;
+    private boolean blockade ;
+    private int cost;
+    private int decks;
+    private int cannons;
    
-    public Ship(String nme, int skillLevel, String captain, String type, boolean battle, boolean skirmish, boolean blockade)
+    public Ship(String nme, int skillLevel, String captain, String type, boolean battle, boolean skirmish, boolean blockade,int cost, int decks, int cannons)
     {
         name = nme;
         this.skillLevel = skillLevel;
         state = ShipState.RESERVE;
         this.captain = captain;
         this.type = type;
+        this.battle = battle;
+        this.skirmish = skirmish;
+        this.blockade = blockade;
+        this.cost = cost;
+        this.decks = decks;
+        this.cannons = cannons;
          
     }
     
@@ -50,20 +59,61 @@ public class Ship {
         return state;
     }
     
-   public boolean battle()
+    public boolean getBattle()
     {
-       return battle;
+        return battle;
     }
-
-    public boolean skirmish()
+    
+    public boolean getSkirmish()
     {
         return skirmish;
     }
-
-    public boolean blockade()
+    
+    public int getCost()
+    {
+        return cost;
+    }
+    
+    public boolean getBlockade()
     {
         return blockade;
     }
+    
+    public void setCost(String type)
+    {
+        if(type.equalsIgnoreCase("Man-O-War"))
+        {
+            if(decks <= 2)
+            {
+                cost = 300;
+            }
+            else
+            {
+                cost = 500;
+            }
+        }
+        
+        else if(type.equalsIgnoreCase("Frigate"))
+        {
+            int amount = cannons * 10;
+            cost = amount;
+        }
+        
+        else if(type.equalsIgnoreCase("Sloop"))
+        {
+            cost = cost;
+        }
+        
+        else
+        {
+            cost = 0;
+        }
+    
+    
+    
+    
+    }
+        
     
     public String toString()
     {
@@ -71,7 +121,8 @@ public class Ship {
         s+= "Name of ship: " + name + "\n" + "Name of captain: " + captain
              + "\n" + "Skill Level :" + skillLevel + "\n"
              + "Type of ship: " + type + "\n" + "Battle: " + battle + "\n" + 
-            "Skirmish: " + skirmish + "\n" + "Blockade: " + blockade + "\n" state.toString() + "\n";
+                "Skirmish: " + skirmish + "\n" + "Blockade: " + blockade + "\n" + "Cost: " + cost + "\n" + 
+                state.toString() + "\n";
         return s;
     }
         
